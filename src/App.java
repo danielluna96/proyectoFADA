9cebcc85a5e5efaf4fb6e93079f05e2b344f23b3
Daniel Luna <daniel.luna@correounivalle.edu.co>
2021-10-14 10:47:15 -0500

    Added algoritmo1voraz

--- src/App.java
+++ src/App.java
@@ -3,6 +3,7 @@ import java.sql.Time;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.Random;
 import java.util.regex.Pattern;
 import java.util.ArrayList;
 import java.util.Date;
@@ -11,18 +12,33 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         /*//Solo para realizar pruebas rapidas
-        Actividad  actividad1 = new Actividad("Actividad 1", convertirADate("2:00"), convertirADate("5:00"));
-        Actividad  actividad2 = new Actividad("Actividad 2", convertirADate("4:00"), convertirADate("22:00"));
-        Actividad  actividad3 = new Actividad("Actividad 3", convertirADate("22:00"), convertirADate("24:00"));
-        Actividad  actividad4 = new Actividad("Actividad 4", convertirADate("22:00"), convertirADate("23:00"));
-        Actividad  actividad5 = new Actividad("Actividad 5", convertirADate("23:00"), convertirADate("24:00"));
+        Actividad  actividad5 = new Actividad("Actividad 1", convertirADate("2:00"), convertirADate("5:00"));
+        Actividad  actividad4 = new Actividad("Actividad 2", convertirADate("5:00"), convertirADate("7:00"));
+        Actividad  actividad3 = new Actividad("Actividad 3", convertirADate("8:00"), convertirADate("20:00"));
+        Actividad  actividad2 = new Actividad("Actividad 4", convertirADate("22:00"), convertirADate("23:00"));
+        Actividad  actividad1 = new Actividad("Actividad 5", convertirADate("23:00"), convertirADate("24:00"));
         List<Actividad> listaActividades = new ArrayList<Actividad>(List.of(actividad1, actividad2, actividad3, actividad4, actividad5));
         */
-                
+
+        //generarActividades(30);
+
         List<Actividad> listacargadaActividades = new ArrayList<Actividad>(cargar());
         Robot robotbase = new Robot(0, new ArrayList<Actividad>());
         Robot robotoptimo = new Robot(0, new ArrayList<Actividad>());
-        algoritmo1(robotbase, listacargadaActividades, false, robotoptimo);
+        //Variables para determinar el tiempo de ejecución
+        long tiempoInicio, tiempoFin, tiempo;
+
+        //Se toma el tiempo de inicio
+        tiempoInicio = System.currentTimeMillis(); 
+        //algoritmo1(robotbase, listacargadaActividades, false, robotoptimo);
+        algoritmo1voraz(robotbase, listacargadaActividades, robotoptimo , convertirADate("00:00"));
+        
+
+        //Una vez realizado el calculo se procede a determinar la diferencia
+        tiempoFin = System.currentTimeMillis();
+        tiempo = tiempoFin - tiempoInicio;
+        mostrar("Tiempo de ejecución en milisegundos: " + tiempo); 
+ 
         mostrar(robotoptimo.toString());
         guardarUno(robotoptimo);
     }
@@ -51,6 +67,37 @@ public class App {
                 }
             }
         }
+
+    public static void algoritmo1voraz(Robot robotbase, List<Actividad> listaActividades, Robot robotoptimo ,Date horaComprobar) throws Exception{
+        if(horaComprobar.equals(convertirADate("24:00"))) {
+                robotoptimo.clear();
+                for (Actividad actividad : robotbase.getActividades()) {
+                        robotoptimo.agregarActividad(actividad);
+                }
+        } else {
+            List<Actividad> actividadesSeleccionadas = new ArrayList<>();
+            for(int index=0; index < listaActividades.size(); index++){
+                if(listaActividades.get(index).getHoraInicio().equals(horaComprobar)){
+                    actividadesSeleccionadas.add(listaActividades.get(index));
+                    listaActividades.remove(index);
+                }
+            }
+            if(!actividadesSeleccionadas.isEmpty()){
+                Actividad actividadLarga = new Actividad("", convertirADate("00:00"), horaComprobar);
+                for(Actividad actividad : actividadesSeleccionadas){
+                    if(actividad.getHoraFin().getTime() > actividadLarga.getHoraFin().getTime()){
+                        actividadLarga = actividad;
+                    }
+                }
+                robotbase.agregarActividad(actividadLarga);
+                horaComprobar = actividadLarga.getHoraFin();
+                algoritmo1voraz(robotbase, listaActividades, robotoptimo, horaComprobar);
+            } else {
+                horaComprobar = new Time(new Date(horaComprobar.getTime() + (1* 3600000)).getTime());
+                algoritmo1voraz(robotbase, listaActividades, robotoptimo, horaComprobar);
+            }
+        }
+    }
 
     public static void mostrar(String cadena){
         System.out.println(cadena);
@@ -99,6 +146,33 @@ public class App {
         System.out.println(e);
     }
     return listaActividades;
+    }
+
+    public static void generarActividades(int n){
+        try{
+            String path="entrada.txt";
+            File file = new File(path);
+
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            FileWriter fw = new FileWriter(file.getAbsoluteFile());
+            BufferedWriter bw = new BufferedWriter(fw);
+            
+            bw.write(Integer.toString(n));
+            bw.newLine();
+            for(int index=0; index < n; index++){
+                int a, b;
+                a = new Random().nextInt(1, 24+1);
+                b = new Random().nextInt(0, a);
+                bw.write("Actividad"+Integer.toString(index+1) + " " + b + ":00 " + a + ":00");
+                bw.newLine();
+            }
+            bw.close();
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
     }
 
     public static void guardarUno(Robot robotOptimo){
